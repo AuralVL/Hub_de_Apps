fd7bcdc08e51f32ac4ccfe281cfbb7a0725a7299
Laura VL <laura_villarreal.l@hotmail.com>
2023-02-13 17:36:55 +0100

    ft: pokeapi

--- pages/Pokemon/Pokemon.js
+++ pages/Pokemon/Pokemon.js
@@ -2,22 +2,18 @@ import "./Pokemon.css";
 
 const template = () => 
     `
-    <section id="poke">
+    <section>
         <h1>PokeApi</h1>
         <div>
             <input type="text" id="searchInput" placeholder="Buscar Pokémon">
             <button id="searchBtn">Buscar</button>
         </div>
         <div id="pokeContainer">
-
         </div>
     </section>
     `
 ;
 
-export const printTemplate = () => {
-    document.querySelector("#app").innerHTML = template();
-};
 let dataList = [];
 
 const getData = async () => {
@@ -26,15 +22,51 @@ const getData = async () => {
             //Primero capturamos los datos en crudo
             const rawData = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
             //También es asíncrono
-            const toJson = await rawData.json();
-            dataList.push(toJson);
+            const dataToJson = await rawData.json();
+            dataList.push(dataToJson);
         } catch (error) {
             console.log('Ha habido un error capturándolos a todos')
-        }
+        };
     };
+    mapData(dataList);
 };
 
-getData();
+//Mapear 
+const mapData = (pokemons) => {
+    const mappedPokemons = pokemons.map((pokemon) => ({
+        name: pokemon.name,
+        image: pokemon.sprites.other["official-artwork"].front_default,
+        type: pokemon.types,
+        height: pokemon.height,
+        moves: pokemon.moves
+    }));
+    printPokemon(mappedPokemons);
+}
 
-console.log(dataList);
+//Pintar
+const printPokemon = (mappedPokemons) => {
+    const container = document.querySelector("#pokeContainer");
+    mappedPokemons.map((pokemon) => {
+        container.innerHTML += `
+            <div id="card">
+                <img class="imagePokemon" src=${pokemon.image} alt=${pokemon.name}/>
+                <h2>${pokemon.name}</h2>
+                <div>
+                    <p id="type">${pokemon.type[0].type.name}</p>
+                    <div class="moves">
+                        ${pokemon.moves.splice(0, 3).map((move) => (`<p>${move.move.name}</p>`))}
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+export const printTemplate = () => {
+    document.querySelector("#app").innerHTML = template();
+    getData();
+};
+
+
+
 
